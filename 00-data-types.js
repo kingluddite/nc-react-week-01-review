21d47a032cae4b1e8dab1c99d3cd0ea8513e6eef
philip howley <howley.phil@gmail.com>
2022-02-26 10:51:58 -0500

    Review JavaScript data types

---
+++ 00-data-types.js
@@ -0,0 +1,32 @@
+// console.log('hello');
+// string
+const firstName = "Phil";
+
+// number
+const age = 20;
+
+
+const message = `${firstName} is ${age} years old`;
+
+
+// boolean
+const likesSoccer = true;
+
+// undefined
+// const score;
+
+// null
+const score = null;
+// object
+const person = {
+    firstName: 'Phil',
+    lastName: 'Howley',
+    age: 21,
+    greet: function() {
+        //console.log('hello')
+        const test = 'yo';
+        return test;
+    }
+}
+
+console.log(typeof age);
