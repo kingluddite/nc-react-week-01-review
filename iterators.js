3212a42c57226f92022bcf6745c704c18005fcbe
philip howley <howley.phil@gmail.com>
2022-02-26 12:45:58 -0500

    add chaining methods

--- iterators.js
+++ iterators.js
@@ -8,21 +8,21 @@ const officers = [
   
   // You only want to output the id's
   // forEach
-  // const officersIds = [];
+//   const officersIds = [];
   
-  // officers.forEach(officer => {
-  //   officersIds.push(officer.id);
-  // });
-  // console.log(officersIds); // [20, 24, 56, 88]
+//   officers.forEach(officer => {
+//     officersIds.push(officer.id);
+//   });
+//   console.log(officersIds); // [20, 24, 56, 88]
   
   // It would be great if we didn't have to first create an empty array
   // map to the rescue
-  // const mapOfficerIds = officers.map(officer => {
-  //   return officer.id;
-  // });
+//   const mapOfficerIds = officers.map(officer => {
+//     return officer.id;
+//   });
   // refactor
-  // const mapOfficerIds = officers.map(officer => officer.id);
-  // console.log(mapOfficerIds); // [20, 24, 56, 88]
+//   const mapOfficerIds = officers.map(officer => officer.id);
+//   console.log(mapOfficerIds); // [20, 24, 56, 88]
   
   // So how does .map() work?
   // It takes 2 arguments, a callback and an optional context (will be considered as `this` in the callback) not used above
@@ -37,63 +37,63 @@ const officers = [
   
   // example of filter
   // how could we get the accumulated age of all the pilots
-  // const pilots = [
-  //   {
-  //     id: 10,
-  //     name: "Poe Dameron",
-  //     years: 14,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Temmin 'Snap' Wexley",
-  //     years: 30,
-  //   },
-  //   {
-  //     id: 41,
-  //     name: "Tallissan Lintra",
-  //     years: 16,
-  //   },
-  //   {
-  //     id: 99,
-  //     name: "Ello Asty",
-  //     years: 22,
-  //   }
-  // ];
+//   const pilots = [
+//     {
+//       id: 10,
+//       name: "Poe Dameron",
+//       years: 14,
+//     },
+//     {
+//       id: 2,
+//       name: "Temmin 'Snap' Wexley",
+//       years: 130,
+//     },
+//     {
+//       id: 41,
+//       name: "Tallissan Lintra",
+//       years: 16,
+//     },
+//     {
+//       id: 99,
+//       name: "Ello Asty",
+//       years: 22,
+//     }
+//   ];
   
-  // const totalYears = pilots.reduce((accumulator, pilot) => {
-  //   return accumulator + pilot.years;
-  // }, 0);
-  // const totalYears = pilots.reduce((accumulator, pilot) => accumulator + pilot.years, 0);
-  // console.log(totalYears); // 82
+//   const totalYears = pilots.reduce((accumulator, pilot) => {
+//     return accumulator + pilot.years;
+//   }, 0);
+//   const totalYears = pilots.reduce((accumulator, pilot) => accumulator + pilot.years, 0);
+//   console.log(totalYears); // 82
   // we start counting at 0 (could also have used an existing value)
   // after running the callback for each element of the array, reduce will return the final value of our accumulator (82)
   
   // filter()
   // Why? you have an array but you only want a few items from it
   // could we use filter to create one array for Rebels and one array for Imperials?
-  // var pilots = [
-  //   {
-  //     id: 2,
-  //     name: "Wedge Antilles",
-  //     faction: "Rebels",
-  //   },
-  //   {
-  //     id: 8,
-  //     name: "Ciena Ree",
-  //     faction: "Empire",
-  //   },
-  //   {
-  //     id: 40,
-  //     name: "Iden Versio",
-  //     faction: "Empire",
-  //   },
-  //   {
-  //     id: 66,
-  //     name: "Thane Kyrell",
-  //     faction: "Rebels",
-  //   }
-  // ];
-  // const rebels = pilots.filter(pilot => pilot.faction === "Rebels");
-  // const imperials = pilots.filter(pilot => pilot.faction === "Empire");
-  // console.log(`rebels: ${JSON.stringify(rebels)}, imperials: ${JSON.stringify(imperials)}`);
+  var pilots = [
+    {
+      id: 2,
+      name: "Wedge Antilles",
+      faction: "Rebels",
+    },
+    {
+      id: 8,
+      name: "Ciena Ree",
+      faction: "Empire",
+    },
+    {
+      id: 40,
+      name: "Iden Versio",
+      faction: "Empire",
+    },
+    {
+      id: 66,
+      name: "Thane Kyrell",
+      faction: "Rebels",
+    }
+  ];
+  const rebels = pilots.filter(pilot => pilot.faction === "Rebels");
+  const imperials = pilots.filter(pilot => pilot.faction === "Empire");
+  console.log(`rebels: ${JSON.stringify(rebels)}, imperials: ${JSON.stringify(imperials)}`);
   
