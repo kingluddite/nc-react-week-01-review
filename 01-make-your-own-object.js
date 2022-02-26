e733f4e90cbc4f14ad7f86a76d8ccb70b8d040df
philip howley <howley.phil@gmail.com>
2022-02-26 11:03:02 -0500

    Add Object exercise

---
+++ 01-make-your-own-object.js
@@ -0,0 +1,14 @@
+// node.js vs client vs dev tools
+const bicycle = {
+    color: 'blue',
+    electric: false,
+    start() {
+      console.log('You begin to pedal the bike.')
+    },
+  }
+  
+  bicycle
+  bicycle.color
+  bicycle.start
+  bicycle.start()
+  
